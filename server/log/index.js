3678617fd417da540ddbfebf9c3b3cbd865c0c84
Uira Lima <uiralima@gmail.com>
2020-11-07 08:39:01 -0300

    Ajustes no log

--- server/log/index.js
+++ server/log/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 module.exports = {
     log: function(message) {
-        const filename = __dirname + "/log.txt"
+        const filename = __dirname + "/log.log"
         if (!fs.existsSync(filename)) {
             fs.writeFile(filename, message, err => {
                 console.log(err || 'Arquivo salvo!')})
